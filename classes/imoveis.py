b92dcdccc11424cbce7ab70ffc4f761137c30ca9
Daniel Nascimento <danidlx2@gmail.com>
2012-02-16 14:20:36 -0200

    Atualizando Classes e Testes

--- classes/imoveis.py
+++ classes/imoveis.py
@@ -3,17 +3,18 @@
 
 
 class Imoveis(object):
-    def __init__(self,endereco,bairro,area,descricao,proprietario_antigo,preco_minimo):
+    def __init__(self,endereco,bairro,area,descricao,proprietario_atual):
         self.endereco = endereco
         self.bairro = bairro
         self.area = area
         self.descricao = descricao
-        self.proprietario_antigo = proprietario_antigo
-        self.proprietario_atual = "IMOR Tal"        
-        self.preco_minimo = preco_minimo
+        self.proprietario_antigo = "IMOR Tal"
+        self.proprietario_atual = proprietario_atual       
     
 
 class ImovelVendido(Imoveis):   
-    def __init__(self,preco_de_venda):
-        super
-        self.preco_de_venda = preco_de_venda   
+    pass   
+
+
+class ImovelDisponivel(Imoveis):
+    pass
--- classes/teste_imoveis.py
+++ classes/teste_imoveis.py
@@ -4,30 +4,31 @@
 import unittest
 from should_dsl import should
 from imoveis import Imoveis, ImovelVendido
+from proprietario import Proprietario
 
 
 class TesteImoveis(unittest.TestCase):
     def teste_criar_imovel(self):
-        imovel = Imoveis(endereco="Dr. Manoel Landin 247",bairro="Alphaville",area=500.0,descricao="Casa",proprietario_antigo="Nilton",preco_minimo=120000)
+        p = Proprietario(nome="Daniel",cpf="130.978.257-11",endereco="Endereco",telefone="99999999")
+        imovel = Imoveis(endereco="Dr. Manoel Landin 247",bairro="Alphaville",area=500.0,descricao="Casa",proprietario_atual=p)
         imovel.endereco |should| equal_to("Dr. Manoel Landin 247")
         imovel.bairro |should| equal_to("Alphaville")
         imovel.descricao |should| equal_to("Casa")
         imovel.area |should| equal_to(500.0)
-        imovel.proprietario_antigo |should| equal_to("Nilton")
-        imovel.proprietario_atual |should| equal_to("IMOR Tal")
-        imovel.preco_minimo |should| equal_to(120000)
 
 class TesteImovelVendido(unittest.TestCase):
 
     def teste_criar_imovel_vendido(self):
-        imovel = ImovelVendido(endereco="Dr. Manoel Landin 247",bairro="Alphaville",area=500.0,descricao="Casa",proprietario_antigo="Nilton",preco_minimo=120000)
+        p = Proprietario(nome="Daniel",cpf="130.978.257-11",endereco="Endereco",telefone="99999999")
+        imovel = ImovelVendido(endereco="Dr. Manoel Landin 247",bairro="Alphaville",area=500.0,descricao="Casa",proprietario_atual=p)
         imovel.endereco |should| equal_to("Dr. Manoel Landin 247")
         imovel.bairro |should| equal_to("Alphaville")
         imovel.descricao |should| equal_to("Casa")
         imovel.area |should| equal_to(500.0)
-        imovel.proprietario_antigo |should| equal_to("Nilton")
-        imovel.proprietario_atual |should| equal_to("IMOR Tal")
-        imovel.preco_minimo |should| equal_to(120000)
+        imovel.proprietario_atual.nome |should| equal_to("Daniel")
+        imovel.proprietario_antigo |should| equal_to("IMOR Tal")
+
+    
 
 
 if __name__ == "__main__":
