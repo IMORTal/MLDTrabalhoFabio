48413511f99eddc8a16f1d27ba9d9b78f0dc4fce
Daniel Nascimento <danidlx2@gmail.com>
2012-02-17 09:05:58 -0200

    Modificações consideráveis na lógica do projeto - Remoção da classe banco

--- classes/imoveis.py
+++ classes/imoveis.py
@@ -2,19 +2,31 @@
 #-*- coding:utf-8 -*-
 
 
-class Imoveis(object):
-    def __init__(self,endereco,bairro,area,descricao,proprietario_atual):
+class Imovel(object):
+    imovel_vendido = []
+    imovel_disponivel = []
+    def __init__(self,endereco,bairro,area,descricao,proprietario_atual,estado):
         self.endereco = endereco
         self.bairro = bairro
         self.area = area
         self.descricao = descricao
         self.proprietario_antigo = "IMOR Tal"
-        self.proprietario_atual = proprietario_atual       
+        self.proprietario_atual = proprietario_atual
+        if estado == "Disponivel":
+            Imovel.adicionar_imoveis_disponiveis(self)
+        else:
+            Imovel.adicionar_imoveis_vendidos(self)
     
+    
+    def retornar_imoveis_vendidos(self):
+        return Imovel.imovel_vendido
+    
+    @staticmethod
+    def adicionar_imoveis_vendidos(imovelvendido):
+        Imovel.imovel_vendido.append(imovelvendido)
+    
+    @staticmethod
+    def adicionar_imoveis_disponiveis(imoveldisponivel):
+        Imovel.imovel_disponivel.append(imoveldisponivel)
 
-class ImovelVendido(Imoveis):   
-    pass   
-
-
-class ImovelDisponivel(Imoveis):
-    pass
+    
--- classes/teste_imoveis.py
+++ classes/teste_imoveis.py
@@ -3,32 +3,40 @@
 
 import unittest
 from should_dsl import should
-from imoveis import Imoveis, ImovelVendido
+from imoveis import Imovel
 from proprietario import Proprietario
 
 
 class TesteImoveis(unittest.TestCase):
     def teste_criar_imovel(self):
         p = Proprietario(nome="Daniel",cpf="130.978.257-11",endereco="Endereco",telefone="99999999")
-        imovel = Imoveis(endereco="Dr. Manoel Landin 247",bairro="Alphaville",area=500.0,descricao="Casa",proprietario_atual=p)
-        imovel.endereco |should| equal_to("Dr. Manoel Landin 247")
-        imovel.bairro |should| equal_to("Alphaville")
-        imovel.descricao |should| equal_to("Casa")
-        imovel.area |should| equal_to(500.0)
-
-class TesteImovelVendido(unittest.TestCase):
-
-    def teste_criar_imovel_vendido(self):
-        p = Proprietario(nome="Daniel",cpf="130.978.257-11",endereco="Endereco",telefone="99999999")
-        imovel = ImovelVendido(endereco="Dr. Manoel Landin 247",bairro="Alphaville",area=500.0,descricao="Casa",proprietario_atual=p)
+        imovel = Imovel(endereco="Dr. Manoel Landin 247",bairro="Alphaville",area=500.0,descricao="Casa",proprietario_atual=p,estado="Disponivel")
         imovel.endereco |should| equal_to("Dr. Manoel Landin 247")
         imovel.bairro |should| equal_to("Alphaville")
         imovel.descricao |should| equal_to("Casa")
         imovel.area |should| equal_to(500.0)
         imovel.proprietario_atual.nome |should| equal_to("Daniel")
-        imovel.proprietario_antigo |should| equal_to("IMOR Tal")
+        imove.retornar_imoveis_disponiveis() |should| equal_to(1)
 
-    
+    def teste_retornar_imoveis_vendidos(self):
+        p = Proprietario(nome="Daniel",cpf="130.978.257-11",endereco="Endereco",telefone="99999999")        
+        imovel = Imovel(endereco="Dr. Manoel Landin 247",bairro="Alphaville",area=500.0,descricao="Casa",proprietario_atual=p,estado="Vendidos")
+        imovel.retornar_imoveis_vendidos() |should| have(1).itens             
+        imovel.retornar_imoveis_vendidos()[0].bairro |should| equal_to("Alphaville")
+        imovel.retornar_imoveis_vendidos()[0].endereco |should| equal_to("Dr. Manoel Landin 247")
+        imovel.retornar_imoveis_vendidos()[0].area |should| equal_to(500.0)
+        imovel.retornar_imoveis_vendidos()[0].descricao |should| equal_to("Casa")
+        imovel.retornar_imoveis_vendidos()[0].proprietario_atual.nome |should| equal_to("Daniel")
+
+    def teste_retornar_imoveis_disponiveis(self):
+        p = Proprietario(nome="Maickon",cpf="321.422.032-13",endereco="Endereco",telefone="99999999")        
+        imovel = Imovel(endereco="28 de Março",bairro="Centro",area=700.0,descricao="Casa",proprietario_atual=p,estado="Disponivel")
+        imovel.retornar_imoveis_disponiveis() |should| have(2).itens             
+        imovel.retornar_imoveis_disponiveis()[1].bairro |should| equal_to("Centro")
+        imovel.retornar_imoveis_disponiveis()[1].endereco |should| equal_to("28 de Março")
+        imovel.retornar_imoveis_disponiveis()[1].area |should| equal_to(500.0)
+        imovel.retornar_imoveis_disponiveis()[1].descricao |should| equal_to("Casa")
+        imovel.retornar_imoveis_disponiveis()[1].proprietario_atual.nome |should| equal_to("Maickon")
 
 
 if __name__ == "__main__":
